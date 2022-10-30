72b4dd7483d232fb2b06a2925d90c0486d54cec1
habiba x <habiba20191700207@cis.asu.edu.eg>
2022-10-30 19:23:00 +0200

    quantization added

--- DSPComponents/Algorithms/QuantizationAndEncoding.cs
+++ DSPComponents/Algorithms/QuantizationAndEncoding.cs
@@ -21,7 +21,60 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
+            OutputIntervalIndices = new List<int>();
+            OutputEncodedSignal = new List<string>();
+            OutputSamplesError = new List<float>();
+            List<float> result = new List<float>();
+            List<float> midpoint = new List<float>();
+            float max = InputSignal.Samples.Max();
+            float min = InputSignal.Samples.Min();
+            List<float> samples = new List<float>();
+
+
+            if (InputNumBits == 0)
+            {
+                InputNumBits = (int)Math.Log(InputLevel, 2);
+            }
+            if (InputLevel == 0)
+            {
+                InputLevel = (int)Math.Pow(2, InputNumBits);
+            }
+           
+
+            float Delta = (max - min) / InputLevel;
+    
+            result.Add(min);
+            for (int i = 1; i <= InputLevel; i++)
+            {
+                result.Add(result[i - 1] + Delta);
+            }
+            for (int i = 0; i < InputLevel; i++)
+            {
+                midpoint.Add((result[i] + result[i + 1]) / 2);
+            }
             
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                for (int j = 0; j < InputLevel; j++)
+                {
+                    if (InputSignal.Samples[i] >= result[j] && InputSignal.Samples[i] <= result[j + 1] + 0.0001)
+                    {
+                        samples.Add(midpoint[j]);
+                        OutputEncodedSignal.Add(Convert.ToString(j, 2).PadLeft(InputNumBits, '0'));
+                        OutputIntervalIndices.Add(j + 1);
+                        break;
+
+                    }
+                }
+            }
+            OutputQuantizedSignal = new Signal(samples, false);
+
+            List<float> samplerror = new List<float>();
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                samplerror.Add(OutputQuantizedSignal.Samples[i] - InputSignal.Samples[i]);
+            }
+            OutputSamplesError = samplerror;
         }
     }
 }
