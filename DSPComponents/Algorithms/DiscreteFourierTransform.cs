a21177b3c88085cfaa790f33b3a0494841ff5032
habiba x <habiba20191700207@cis.asu.edu.eg>
2022-11-07 22:30:36 +0200

    DFFT and IDFFT added

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
@@ -15,7 +16,32 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputFreqDomainSignal = new Signal(new List<float>(), false);
+            int N = InputTimeDomainSignal.Samples.Count;
+            List<float> Amplitude = new List<float>();
+            List<float> Phase = new List<float>();
+            //k constant term in samples
+            for (int k = 0; k < N; k++)
+            {
+                double sum_of_complex = 0, sum_of_real = 0;
+                //n samples
+                for (int n = 0; n < N; n++)
+                {
+                    double ePower = -(n * 2 * k * Math.PI) / N;
+                    sum_of_real += (Math.Cos(ePower) * InputTimeDomainSignal.Samples[n]);
+                    sum_of_complex += (Math.Sin(ePower) * InputTimeDomainSignal.Samples[n]);
+                }
+
+                //amplitude 
+                Amplitude.Add((float)Math.Sqrt((Math.Pow(sum_of_real, 2) + Math.Pow(sum_of_complex, 2))));
+
+                //phase 
+                Phase.Add((float)Math.Atan2(sum_of_complex, sum_of_real));
+
+            }
+            OutputFreqDomainSignal.FrequenciesAmplitudes = Amplitude;
+            OutputFreqDomainSignal.FrequenciesPhaseShifts = Phase;
+
         }
     }
 }
--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
@@ -14,7 +15,30 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<Complex> complex = new List<Complex>();
+            List<Complex> result = new List<Complex>();
+            int N = InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+
+            for (int n = 0; n < N; n++)
+            {
+                result.Add(0);
+                for (int k = 0; k < N; k++)
+                {
+                    complex.Add(Complex.FromPolarCoordinates(InputFreqDomainSignal.FrequenciesAmplitudes[k], InputFreqDomainSignal.FrequenciesPhaseShifts[k]));
+                    double epower = (2 * k * Math.PI * n) / N;
+                    result[n] += complex[k] * (Math.Cos(epower) + Complex.ImaginaryOne * Math.Sin(epower));
+                }
+                result[n] /= N;
+            }
+
+            List<float> original_samples = new List<float>();
+
+            for (int i = 0; i < N; i++)
+            {
+                original_samples.Add((float)result[i].Real);
+            }
+            OutputTimeDomainSignal = new Signal(original_samples, false);
+
         }
     }
 }
