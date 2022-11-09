3e4e404108c5c4233048dbafccd7b630d534f01e
habiba x <habiba20191700207@cis.asu.edu.eg>
2022-11-09 19:09:47 +0200

    test casses added

--- DSPComponentsUnitTest/SinCosTestCases.cs
+++ DSPComponentsUnitTest/SinCosTestCases.cs
@@ -21,7 +21,7 @@ namespace DSPComponentsUnitTest
 
             obj.Run();
 
-            var expectedOutput = UnitTestUtitlities.LoadSignal("TestingSignals/Sin.ds");
+            var expectedOutput = UnitTestUtitlities.LoadSignal("C:/Users/Habiba Mohamady/Source/Repos/DigitalSignalProcessing/DSPComponentsUnitTest/TestingSignals/Sin.ds");
 
             Assert.IsTrue(UnitTestUtitlities.SignalsSamplesAreEqual(expectedOutput.Samples, obj.samples));
 
@@ -37,7 +37,7 @@ namespace DSPComponentsUnitTest
             obj.PhaseShift = 2.35619449019235f;
 
             obj.Run();
-            var expectedOutput = UnitTestUtitlities.LoadSignal("TestingSignals/Cos.ds");
+            var expectedOutput = UnitTestUtitlities.LoadSignal("C:/Users/Habiba Mohamady/Source/Repos/DigitalSignalProcessing/DSPComponentsUnitTest/TestingSignals/Cos.ds");
             Assert.IsTrue(UnitTestUtitlities.SignalsSamplesAreEqual(expectedOutput.Samples, obj.samples));
 
 
