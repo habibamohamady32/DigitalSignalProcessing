5132555c02e3836cf0ea437916198cc7d71df5b8
habiba x <habiba20191700207@cis.asu.edu.eg>
2022-11-28 21:38:16 +0200

    correlation

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -13,109 +13,56 @@ namespace DSPAlgorithms.Algorithms
         public Signal InputSignal2 { get; set; }
         public List<float> OutputNonNormalizedCorrelation { get; set; }
         public List<float> OutputNormalizedCorrelation { get; set; }
-
         public override void Run()
+        {
+            if (InputSignal2 == null)
+                autoCorrelation();
+            else
+                crossCorrelation();
+        }
+        public void autoCorrelation()
+        {
+            InputSignal2 = new Signal(new List<float>(), false);
+            for (int i = 0; i < InputSignal1.Samples.Count; i++)
+            {
+                InputSignal2.Samples.Add(InputSignal1.Samples[i]);
+            }
+            crossCorrelation();
+        }
+        public void crossCorrelation()
         {
             OutputNonNormalizedCorrelation = new List<float>();
             OutputNormalizedCorrelation = new List<float>();
-            List<float> copy1 = new List<float>();
-            List<float> copy2 = new List<float>();
             int N = InputSignal1.Samples.Count;
-
-            for (int i = 0; i < N; i++)
-                copy1.Add(InputSignal1.Samples[i]);
-            int ishift = 0;
-            //auto-correlation
-            if (InputSignal2 == null)
+            int isshifted;
+            double x = 0, y = 0, normlize, corr;
+            for (int i = 0; i < InputSignal1.Samples.Count; i++)
             {
-                double normal1 = 0, one = 0, two = 0;
-                for (int i = 0; i < N; i++)
-                {
-                    // signal_samples_summation += signal1_samples[i] * signal1_samples[i];
-                    // signal_samples_copy_summation += signal1_samples_copy[i] * signal1_samples_copy[i];
-                    one += Math.Pow(copy1[i], 2);
-                    two += Math.Pow(copy1[i], 2);
-
-                }
-                normal1 = Math.Sqrt(one * two) / N;
-
-                if (InputSignal1.Periodic == true)
-                {
-                    for (int i = 0; i < N; i++)
-                    {
-                        double s = 0;
-                        ishift = i;
-                        for (int j = 0; j < N; j++)
-                        {
-                            s += copy1[ishift++] * copy1[j];
-                            ishift %= N;
-                        }
-                        float tmp = (float)s / N;
-                        OutputNonNormalizedCorrelation.Add(tmp);
-                        OutputNormalizedCorrelation.Add((float)(tmp / normal1));
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < N; i++)
-                    {
-                        double s = 0;
-                        if (i != 0)
-                        {
-                            for (int j = 0; j < N - 1; j++)
-                            {
-                                copy1[j] = copy1[j + 1];
-                                s += copy1[j] * InputSignal1.Samples[j];
-                            }
-                            copy1[N - 1] = 0;
-                        }
-                        else
-                        {
-                            for (int j = 0; j < N; j++)
-                            {
-                                s += InputSignal1.Samples[j] * InputSignal1.Samples[j];
-                            }
-                        }
-                        float tmp = (float)s / N;
-                        OutputNonNormalizedCorrelation.Add(tmp);
-                        OutputNormalizedCorrelation.Add((float)(tmp / normal1));
-                    }
-                }
+                x += Math.Pow(InputSignal1.Samples[i], 2);
+                y += Math.Pow(InputSignal2.Samples[i], 2);
             }
-
-            else// cross-correlation
+            normlize = Math.Sqrt(x * y) /N;
+            for (int i = 0; i < InputSignal1.Samples.Count; i++)
             {
-                OutputNonNormalizedCorrelation = new List<float>();
-                OutputNormalizedCorrelation = new List<float>();
-                int steps;
-                double x = 0, y = 0, norm, corr;
-                for (int i = 0; i < InputSignal1.Samples.Count; i++)
+                corr = 0;
+                isshifted = -1;
+                for (int j = 0; j < InputSignal1.Samples.Count; j++)
                 {
-                    x += Math.Pow(InputSignal1.Samples[i], 2);
-                    y += Math.Pow(InputSignal2.Samples[i], 2);
-                }
-                norm = Math.Sqrt(x * y) / InputSignal1.Samples.Count;
-                for (int i = 0; i < InputSignal1.Samples.Count; i++)
-                {
-                    corr = 0;
-                    steps = -1;
-                    for (int j = 0; j < InputSignal1.Samples.Count; j++)
+                    if (j + i <= InputSignal1.Samples.Count - 1)
+                        corr += InputSignal1.Samples[j] * InputSignal2.Samples[j + i];
+                    else if (InputSignal1.Periodic == true)
                     {
-                        if (j + i <= InputSignal1.Samples.Count - 1)
-                            corr += InputSignal1.Samples[j] * InputSignal2.Samples[j + i];
-                        else if (InputSignal1.Periodic == true)
-                        {
-                            steps++;
-                            corr += InputSignal1.Samples[j] * InputSignal2.Samples[steps];
-                        }
-                        else if (InputSignal1.Periodic == false)
-                            corr += InputSignal1.Samples[j] * 0;
+                        isshifted++;
+                        corr += InputSignal1.Samples[j] * InputSignal2.Samples[isshifted];
                     }
-                    OutputNonNormalizedCorrelation.Add((float)corr / InputSignal1.Samples.Count);
-                    OutputNormalizedCorrelation.Add((float)((corr / InputSignal1.Samples.Count) / norm));
+                    else if (InputSignal1.Periodic == false)
+                        corr += InputSignal1.Samples[j] * 0;
                 }
+                float nonNormalizedResult = (float)(corr / N);
+                OutputNonNormalizedCorrelation.Add((float)nonNormalizedResult );
+                OutputNormalizedCorrelation.Add((float)(nonNormalizedResult/normlize));
             }
-            
         }
+
     }
 }
